dd5876432260904430a89b022dfc3e796e087b19
Osvaldo Ibañez <osva552920@gmail.com>
2019-09-24 10:37:21 -0500

    Todas las clases un un solo archivo

--- Animal.java
+++ Animal.java
@@ -2,3 +2,16 @@ package Equipo7_Practica7;
 public abstract class Animal{
   public abstract String getName(); 
 }
+
+class Flamingo extends Animal {
+  public String getName() {
+    return "Flamingo";
+  }
+}
+
+class Bird extends Flamingo{
+}
+
+
+
+}
