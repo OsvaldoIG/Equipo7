57e98d6cdb8c019c5b7d96e02e69385b9a431e94
Osvaldo Ibañez <osva552920@gmail.com>
2019-09-24 10:06:42 -0500

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,13 +1,4 @@
-package Equipo7-Practica7;
+package Equipo7_Practica7;
 public abstract class Animal{
   public abstract String getName(); 
-}
-
-public class Bird extends Animal{
-}
-
-public class Flamingo extends Bird {
-  public String getName() {
-    return "Flamingo";
-  }
 }
