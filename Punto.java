647a5bda90e149638af41014fbfee0b36d9b0be7
LeoonardooCRZ <42351420+LeoonardooCRZ@users.noreply.github.com>
2019-09-24 09:34:53 -0500

    se agregó la clase circulo que hereda de Punto

--- Punto.java
+++ Punto.java
@@ -31,3 +31,40 @@ public class Punto{
   public String toString(){
     return "["+x+", "+y+"]";
   }
+
+public class Circulo extends Punto{
+  private doube radio;
+  
+  public  Circulo(){
+  }
+  
+  public Circulo(int valorX, int valorY, double valorRadio){
+    x = valorX;
+    y = valorY;
+    setRadio(valorRadio);
+  }
+  
+  public void setRadio(double valorRadio){
+    radio = (valorRadio < 0.0 ? 0.0 : valorRadio);
+  }
+  
+  public double getRadio(){
+    return radio;
+  }
+  
+  public double getDiametro(){
+     return 2 * radio;
+  }
+  
+  public double getCircunferencia(){
+      return Math.PI * getDiametro();
+  }
+  
+  public double getArea(){
+    return Math.PI * radio * radio;
+  }
+  
+  public String toString(){
+    return "Centro = ["+x+", "+y+"]; Radio = "+radio;
+  }
+}
