dff2e8cf502bf357666ed670a5756f575b97ff9d
PizzaDude007 <42350876+PizzaDude007@users.noreply.github.com>
2019-10-01 09:48:54 -0500

    Update RelacionJerarquica.java

--- RelacionJerarquica.java
+++ RelacionJerarquica.java
@@ -2,9 +2,9 @@ public class RelacionJerarquica{
   public static void main (String[] args){
     Punto punto = new Punto(30, 50);
     //Circulo circulo = new Circulo(120, 89, 2.7);
-    Circulo circulo = (circulo)punto;
+    Circulo circulo = (Circulo)punto;
     
-    System.out.pritnln("Llamado a toString de Punto con referencia a la superficie"
+    System.out.println("Llamado a toString de Punto con referencia a la superficie"
     +"apuntando al objeto de la superclase:\n"+punto.toString());
     
     System.out.println("Llamado a toString de Cicrulo con referencia a la subclase"
