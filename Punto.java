5eeefdeb117c947386114f5e61bb14ecbd8ac778
LeoonardooCRZ <42351420+LeoonardooCRZ@users.noreply.github.com>
2019-10-01 09:24:38 -0500

    Create Punto.java

---
+++ Punto.java
@@ -0,0 +1,36 @@
+public class Punto extends Figura{
+ private int x;
+ private int y;
+
+
+ public Punto(){}
+
+ public Punto(int valorX, int valorY){
+  x = valorX;
+  y = valorY;
+ }
+
+ public void setX(int valorX){
+  x = valorX;
+ }
+
+ public int getX(){
+  return x;
+ }
+
+ public void setY(int valorY){
+  y = valorY;
+ }
+
+ public int getY(){
+  return y;
+ }
+
+ public String toString(){
+  return "[" + getX() + ", " + getY() + "]";
+ }
+
+ public abstract String obtenerNombre(){
+  return "Punto";
+ }
+}
