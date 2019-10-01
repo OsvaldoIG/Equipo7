1e35f66caf059694c5bdf43bab5816ea39dcf427
LeoonardooCRZ <42351420+LeoonardooCRZ@users.noreply.github.com>
2019-09-30 22:09:53 -0500

    Update ClienteCilindro.java

--- ClienteCilindro.java
+++ ClienteCilindro.java
@@ -1,7 +1,6 @@
-package Equipo7-Practica7;
-
+package Equipo7_Practica7;
 public class ClienteCilindro{
-  Cilindro cilindro = new Cilindro();
-  cilindro.setAltura(15);
-  cilindro.getArea();
+  Cilindro cilindro = new Cilindro(0,0,5.2,10);
+  double area = cilindro.getAreaC();
+  System.out.println(area);
 }
