c140949919f1ec26da98c8a2667944d27a851cca
Osvaldo Ibañez <osva552920@gmail.com>
2019-10-15 10:16:49 -0500

    Update EmpleadoAsalariado.java

--- EmpleadoAsalariado.java
+++ EmpleadoAsalariado.java
@@ -1,1 +1,22 @@
+package my.equipo7.nomina;
+
+public class EmpleadoAsalariado extends Empleado{
+    private double salarioSemanal;
+    public EmpleadoAsalariado(String nombre, String apellido, String numeroSS){
+        super(nombre,apellido,numeroSS);
+    }
+    
+    public String ingresos(){
+        return "Salario Semanal "+getSalarioSemanal();
+    }
+    
+    public void setSalarioSemanal(double sueldo){
+        this.salarioSemanal = sueldo;
+    }
+    
+    public double getSalarioSemanal(){
+        return salarioSemanal;
+    }
+    
+}
 
