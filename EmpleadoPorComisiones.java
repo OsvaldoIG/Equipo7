26d5548f7b72657f83a128f39609e92e048f206d
PizzaDude007 <42350876+PizzaDude007@users.noreply.github.com>
2019-10-15 10:19:10 -0500

    Update EmpleadoPorComisiones.java

--- EmpleadoPorComisiones.java
+++ EmpleadoPorComisiones.java
@@ -1,5 +1,35 @@
-package my.equipo7.nomina;
+package com.equipo7.nomina;
 
-public class EmpleadoPorComisiones extends Emepleado{
-
+/**
+ *
+ * @author poo02alu37
+ */
+public class EmpleadoPorComision extends Empleado{
+    private int ventasTotales;
+    private float tasaComision;
+    
+    public EmpleadoPorComision (String nombre,String apellido,String numSS, double sueldo){ 
+        super(nombre, apellido, numSS);
+        super.sueldo = sueldo;
+    }
+    
+    public void setVentasTotales(int ventas){
+        this.ventasTotales = ventas;
+    }
+    
+    public void setTasaComision(float tasa){
+        this.tasaComision = tasa;
+    }
+    
+    public int getVentasTotales(){
+        return ventasTotales;
+    }
+    
+    public float getTasaComision(){
+        return tasaComision;
+    }
+    
+    public String ingresos(){
+        return "Los ingresos son de : "+(ventasTotales * tasaComision);
+    }
 }
