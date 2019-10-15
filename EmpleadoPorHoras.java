c3d92e81ba104db37fb20dcd2959009cd8e929f5
LeoonardooCRZ <42351420+LeoonardooCRZ@users.noreply.github.com>
2019-10-15 10:16:11 -0500

    Update EmpleadoPorHoras.java

--- EmpleadoPorHoras.java
+++ EmpleadoPorHoras.java
@@ -1,1 +1,28 @@
+package com.equipo7.nomina;
+public classs EmpleadoPorHoras extends Empleado{
+	private double sueldo;
+	private int horas;
 
+	public EmpleadoPorHoras(String nombre, String apellido, String numeroSS){
+		super(nombre,apellido,numeroSS);
+	}
+
+	public String ingresos(){
+		return "Sueldo: "+getSueldo()*getHoras();
+	}
+	public void setSueldo( double sueldo ){
+		this.sueldo = sueldo;
+	}
+
+	public void setHoras( int horas ){
+		this.horas = horas;
+	}
+
+	public double getSueldo(){
+		return sueldo;
+	}
+
+	public int getHoras(){
+		return horas;
+	}
+}
