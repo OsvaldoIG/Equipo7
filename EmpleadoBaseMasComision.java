e6d9e2dd1457d9794e3366a11d54aa8ce31f0370
PizzaDude007 <42350876+PizzaDude007@users.noreply.github.com>
2019-10-15 10:23:45 -0500

    Update EmpleadoBaseMasComision.java

--- EmpleadoBaseMasComision.java
+++ EmpleadoBaseMasComision.java
@@ -1,1 +1,22 @@
+package com.equipo7.nomina;
 
+/**
+ *
+ * @author poo02alu37
+ */
+public class EmpleadoBaseMasComision extends EmpleadoPorComision{
+    private double salarioBase;
+    
+    EmpleadoBaseMasComision(String nombre, String apellido, String numSS, double sueldo){ 
+        super(nombre, apellido, numSS, sueldo);
+    }
+    
+    public double getSalarioBase(){
+        return salarioBase;
+    }
+    
+    public void setSalarioBase(float salario){
+        this.salarioBase = salario;
+    }
+    
+}
