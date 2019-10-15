8e777e24897ae83da6386f59ba6aac723e5e2904
PizzaDude007 <42350876+PizzaDude007@users.noreply.github.com>
2019-10-15 10:01:21 -0500

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -1,5 +1,44 @@
-public class Empleado{
-  public Empleado(){
-  
-  }
+package my.equipo7.nomina;
+
+public abstract class Empleado {
+    String nombre;
+    String apellido;
+    String numSS;
+    double sueldo;
+
+    public Empleado (String nombre, String apellido, String numSS){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numSS = numSS;
+    }
+    
+    public String toString(){
+        return "Nombre: "+apellido+" "+nombre+"\nNumero de Seguridad Social: "+numSS;
+    }
+    
+    public abstract void ingresos();
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+    
+    public void setNumSS(String numSS){
+        this.numSS = numSS;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getApellido(){
+        return apellido;
+    }
+    
+    public String getNumSS(){
+        return numSS;
+    }
 }
